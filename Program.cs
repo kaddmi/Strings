5375af03094b7418e2cfe3560d0ddfe92bbac891
Anastasija Pischik <nastijpischik@gmail.com>
2018-09-18 21:42:48 +0300

    up1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TaskString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextTransformer textTransf = new TextTransformer();
+            string text = Console.ReadLine();
+            Console.WriteLine(textTransf.Transform(text));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TextTransformer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskString
+{
+    class TextTransformer
+    {
+        public string Transform(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return "Some value";
+            else
+            {
+                StringBuilder res = new StringBuilder();
+                foreach (char c in s)
+                {
+                    res.Append(Char.ToUpper(c));
+                }
+                return res.ToString();
+            }
+        }
+    }
+}
